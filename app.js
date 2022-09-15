b91b00ef0b51de2746682e37d923beba9c37c11e
kristinasytenko <108756017+kristinasytenko@users.noreply.github.com>
2022-09-15 13:36:54 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,85 +1,64 @@
-let tg = window.Telegram.WebApp;
+function app() {
+    const buttons = document.querySelectorAll('.button')
+    const cards = document.querySelectorAll('.card')
 
-tg.expand();
+    function filter (category, items) {
+        items.forEach((item) => {
+            const isItemFiltered = !item.classList.contains(category)
+            const isShowAll = category.toLowerCase() === 'all'
+            if (isItemFiltered && !isShowAll) {
+                item.classList.add('anime')
+            } else {
+                item.classList.remove('hide')
+                item.classList.remove('anime')
+            }
+        })
+    }
 
-tg.MainButton.textColor = "#FFFFFF";
-tg.MainButton.color = "#2cab37";
+    buttons.forEach((button) => {
+        button.addEventListener('click', () => {
+            const currentCategory = button.dataset.filter
+            filter(currentCategory, cards)
+        })
+    })
 
+    cards.forEach((card) => {
+        card.ontransitionend = function () {
+            if (card.classList.contains('anime')) {
+                card.classList.add('hide')
+            }
+        }
+    })
+}
 
-let btn1 = document.getElementById("btn1");
-let btn2 = document.getElementById("btn2");
-let btn3 = document.getElementById("btn3");
-let btn4 = document.getElementById("btn4");
-let btn5 = document.getElementById("btn5");
-let btn6 = document.getElementById("btn6");
+app()
 
-let item = "";
+var numCount = document.getElementById('num_count');
+var plusBtn = document.getElementById('button_plus');
+var minusBtn = document.getElementById('button_minus');
+plusBtn.onclick = function() {
+  var qty = parseInt(numCount.value);
+  qty = qty + 1;
+  numCount.value = qty;
+}
+minusBtn.onclick = function() {
+  var qty = parseInt(numCount.value);
+  qty = qty - 1;
+  if (qty == 0) {
+	qty = 1;
+  }
+  numCount.value = qty;
+ 
+  
+}
 
-btn1.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 1!");
-		item = "1";
-		tg.MainButton.show();
-	}
-});
-
-btn2.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 2!");
-		item = "2";
-		tg.MainButton.show();
-	}
-});
-
-btn3.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 3!");
-		item = "3";
-		tg.MainButton.show();
-	}
-});
-btn4.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 4!");
-		item = "4";
-		tg.MainButton.show();
-	}
-});
-btn5.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 5!");
-		item = "5";
-		tg.MainButton.show();
-	}
-});
-btn6.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 6!");
-		item = "6";
-		tg.MainButton.show();
-	}
-});
-
-
-
-Telegram.WebApp.onEvent("mainButtonClicked", function(){
-	tg.sendData(item);
-});
+$(function() {
+	// Owl Carousel
+	var owl = $(".owl-carousel");
+	owl.owlCarousel({
+	  items: 3,
+	  margin: 0,
+	  loop: true,
+	  nav: false
+	});
+  });
